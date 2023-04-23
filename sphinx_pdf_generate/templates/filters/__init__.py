659648cf2018791ea38139e6e1058bc83fa1aba3
Randy Duodu <duodurandy19@gmail.com>
2023-04-23 07:59:24 +0000

    Updated filters/__init__.py

--- sphinx_pdf_generate/templates/filters/__init__.py
+++ sphinx_pdf_generate/templates/filters/__init__.py
@@ -1,15 +1,13 @@
 from typing import Any, Dict
 
-from sphinx_pdf_generate.options import Options
-
 
 class _FilterBase:
-    def __init__(self, options: Options, config: Dict[str, Any]):
+    def __init__(self, options: object, config: Dict[str, Any]):
         self.__options = options
         self.__config = config
 
     @property
-    def options(self) -> Options:
+    def options(self) -> object:
         return self.__options
 
     @property
